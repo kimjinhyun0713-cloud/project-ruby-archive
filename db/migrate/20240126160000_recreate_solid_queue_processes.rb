5e234a26465d6b09dc80fac427510119515bedc6
Jinhyeon Kim <jinhyeonkim@Kim-MacAir.local>
2026-01-26 04:05:31 +0900

    Recreate solid_queue_processes table correctly

---
+++ db/migrate/20240126160000_recreate_solid_queue_processes.rb
@@ -0,0 +1,17 @@
+class RecreateSolidQueueProcesses < ActiveRecord::Migration[7.1]
+  def change
+    drop_table :solid_queue_processes, if_exists: true
+    create_table :solid_queue_processes do |t|
+      t.string :kind, null: false
+      t.json :metadata
+      t.integer :supervisor_id
+      t.integer :pid, null: false
+      t.string :hostname
+      t.string :name, null: false
+      t.datetime :last_heartbeat_at, null: false, index: true
+      t.datetime :created_at, null: false
+      t.index [ :last_heartbeat_at ], name: "index_solid_queue_processes_on_last_heartbeat_at"
+      t.index [ :supervisor_id ], name: "index_solid_queue_processes_on_supervisor_id"
+    end
+  end
+end
