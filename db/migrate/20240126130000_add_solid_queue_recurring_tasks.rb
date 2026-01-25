feaa55feeed80416bd8fe2c6cc6973354adbf6e0
Jinhyeon Kim <jinhyeonkim@Kim-MacAir.local>
2026-01-26 03:23:39 +0900

    Add missing solid_queue_recurring_tasks table

---
+++ db/migrate/20240126130000_add_solid_queue_recurring_tasks.rb
@@ -0,0 +1,20 @@
+class AddSolidQueueRecurringTasks < ActiveRecord::Migration[7.1]
+  def change
+    create_table :solid_queue_recurring_tasks do |t|
+      t.string :key, null: false
+      t.string :schedule, null: false
+      t.string :command, limit: 2048
+      t.string :class_name
+      t.text :arguments
+      t.string :queue_name
+      t.integer :priority, default: 0
+      t.boolean :static, default: true, null: false
+      t.text :description
+
+      t.timestamps
+    end
+
+    add_index :solid_queue_recurring_tasks, :key, unique: true
+    add_index :solid_queue_recurring_tasks, :static
+  end
+end
